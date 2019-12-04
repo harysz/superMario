e735f455737e3679a2bf7af2c4e11c30eb44015e
harysz <arnodlas.valasinas@gmail.com>
2019-12-04 20:36:39 +0200

    adding timing for equal performance with different framerates

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 import Compositor from './Compositor.js';
+import Timer from './timer.js';
 import {loadLevel} from './loaders.js';
 import {createMario} from './mario.js';
 import {loadBackgroundSprites} from './sprites.js';
@@ -6,7 +7,7 @@ import {createSpriteLayer,createBackgroundLayer} from './layers.js';
 import Entity from './entity.js';
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
-context.scale(3,2);
+context.scale(1.5,1.3);
 
 
 Promise.all([
@@ -20,17 +21,28 @@ Promise.all([
     const comp = new Compositor();
     comp.layers.push(createBackgroundLayer(level.backgrounds, backgroundSprites));
 
-    const gravity = 0.5;
+    const gravity = 30;
+    mario.pos.set(64,180);
+    mario.vel.set(200,-600);
+
 
     comp.layers.push(createSpriteLayer(mario));
 
-    function update(time) {
-        console.log(time);
+    const timer = new Time(1/60);
+
+   
+   timer.update = function update(time) {
+        accumulatedTime += (time - lastTime)/1000;
+        while(accumulatedTime > deltaTime){
         comp.draw(context);
-        mario.update();
+        mario.update(deltaTime);
         mario.vel.y += gravity;
+
+        accumulatedTime -= deltaTime;
+        }
         requestAnimationFrame(update);
+        lastTime = time;
     }
 
-    update();
+    update(0);
 });
--- public/js/mario.js
+++ public/js/mario.js
@@ -5,16 +5,15 @@ export function createMario (){
      return loadMarioSprite()
     .then(sprite =>{
     const mario = new Entity();
-    mario.pos.set(64,180);
-    mario.vel.set(2,-10);
+  
 
     mario.draw = function drawMario(context){
         sprite.draw('idle', context, this.pos.x, this.pos.y);
     };
 
-    mario.update = function updateMario(){
-        this.pos.y += this.vel.y;
-        this.pos.x += this.vel.x;
+    mario.update = function updateMario(deltaTime){
+        this.pos.y += this.vel.y * deltaTime;
+        this.pos.x += this.vel.x * deltaTime;
     };
     return mario;
     });
